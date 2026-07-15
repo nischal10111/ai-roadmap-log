fb7b756e9fad69692b6a5da845db7687949a8f1d
nischal10111 <nischalpoudel976@gmail.com>
2026-07-15 06:31:58 -0700

    Add files via upload

---
+++ phase1.day2.py
@@ -0,0 +1,11 @@
+num1= int(input("Enter  first number:"))
+num2 = int (input("Enter second number:"))
+print("Sum:",num1 +num2)
+print("Difference:",num1-num2)
+print("product:",num1*num2)
+print("Division:",num1/num2)
+if num1 > num2:
+    print("num1 is greater than num2")
+else:
+    print("num2 is greater than num1")
+
